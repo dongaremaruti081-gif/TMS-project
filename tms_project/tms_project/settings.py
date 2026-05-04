bdd538702ad63630e1d66c9cbc66c7cf0cfc0967
Maruti Dongare <marutidongare8888@gmail.com>
2026-05-04 11:36:35 +0530

    Fix ALLOWED_HOSTS for Render

--- tms_project/tms_project/settings.py
+++ tms_project/tms_project/settings.py
@@ -27,8 +27,11 @@ DEBUG = False
 import os
 
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "tms-project-jq42.onrender.com"
+]
 
 # Application definition
 
