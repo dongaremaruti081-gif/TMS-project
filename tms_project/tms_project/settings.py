71c27d4c5426d4c0995a902e10b62489a64b86ed
Maruti Dongare <marutidongare8888@gmail.com>
2026-05-04 12:09:57 +0530

    Fix Render 500 error - DB + settings clean

--- tms_project/tms_project/settings.py
+++ tms_project/tms_project/settings.py
@@ -96,11 +96,11 @@ WSGI_APPLICATION = 'tms_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQLDATABASE"),
-        'USER': os.getenv("MYSQLUSER"),
-        'PASSWORD': os.getenv("MYSQLPASSWORD"),
-        'HOST': os.getenv("MYSQLHOST"),
-        'PORT': os.getenv("MYSQLPORT"),
+        'NAME': os.getenv("MYSQLDATABASE", ""),
+        'USER': os.getenv("MYSQLUSER", ""),
+        'PASSWORD': os.getenv("MYSQLPASSWORD", ""),
+        'HOST': os.getenv("MYSQLHOST", ""),
+        'PORT': os.getenv("MYSQLPORT", "3306"),
     }
 }
 # Password validation
