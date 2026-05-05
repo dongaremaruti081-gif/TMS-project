c7083a4d1869737d17338dce8d6881f33cf5fcbe
Maruti Dongare <marutidongare8888@gmail.com>
2026-05-05 11:18:48 +0530

    little

--- tms_project/tms_project/settings.py
+++ tms_project/tms_project/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'tms_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],   # 🔥 हे add कर
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
